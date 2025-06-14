875883d62c5f0a17c0ee18d85aa4a369a48a8bc4
kiskey <59226371+kiskey@users.noreply.github.com>
2025-06-13 19:05:09 -0700

    Update threadProcessor.js

--- src/workers/threadProcessor.js
+++ src/workers/threadProcessor.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const { parseThreadPage } = require('../parser/htmlParser');
 const { parseTitle, normalizeBaseTitle } = require('../parser/titleParser');
 const dataManager = require('../database/dataManager');
-const { searchTv } = require('../utils/tmdb');
+const { searchTv, getTvDetails } = require('../utils/tmdb');
 const config = require('../utils/config');
 const logger = require('../utils/logger');
 
@@ -45,23 +45,29 @@ async function processThread(threadUrl) {
             return;
         }
 
-        // --- NEW METADATA LOGIC ---
-        // 1. Find the TMDb ID for this show
+        // 1. Find the TMDb ID for this show using our title search
         const tmdbResult = await searchTv(baseTitle, year);
         if (!tmdbResult || !tmdbResult.id) {
-            logger.warn({ title: baseTitle, year }, "Could not find TMDb match, cannot process this show.");
+            logger.warn({ title: baseTitle, year }, "Could not find TMDb match, skipping thread.");
             return;
         }
         const tmdbId = tmdbResult.id;
 
-        // 2. Create the show record mapping our internal name to the tmdbId
-        await dataManager.findOrCreateShow(baseTitle, year, tmdbId);
+        // 2. Get the full details from TMDb to find the corresponding IMDb ID
+        const details = await getTvDetails(tmdbId);
+        if (!details || !details.imdbId) {
+            logger.warn({ tmdbId, title: baseTitle }, 'Could not get IMDb ID from TMDb details, cannot create mapping.');
+            return;
+        }
+        const imdbId = details.imdbId;
+
+        // 3. Create the show record in our DB, storing the IMDb -> TMDb mapping
+        await dataManager.findOrCreateShow(tmdbId, imdbId);
         
-        // 3. Add all streams to this TMDb ID's stream list
+        // 4. Add all found streams to this TMDb ID's stream list
         for (const magnetUri of threadData.magnets) {
             const parsedStream = parseTitle(magnetUri);
             if (parsedStream) {
-                // Pass the TMDb ID to the addStream function
                 await dataManager.addStream(tmdbId, parsedStream);
             }
         }
@@ -79,7 +85,6 @@ if (!parentPort) {
 
 parentPort.on('message', async (task) => {
     if (task && task.url) {
-        logger.debug({ url: task.url }, `Worker received task.`);
         await processThread(task.url);
 
         if (global.gc) {
